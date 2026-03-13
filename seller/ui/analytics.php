8b5cfcbd6ee4270f65a1adb0e0919e08053d74d7
chris <christiantitoy@gmail.com>
2026-03-13 22:00:32 +0800

    yesher

--- seller/ui/analytics.php
+++ seller/ui/analytics.php
@@ -23,7 +23,7 @@
             <a href="/seller/ui/dashboard.php" class="nav-item"><i class="fas fa-tachometer-alt"></i><span>Dashboard</span></a>
             <a href="/seller/ui/products.php" class="nav-item"><i class="fas fa-box"></i><span>Products</span></a>
             <a href="/seller/ui/orders.php" class="nav-item"><i class="fas fa-shopping-cart"></i><span>Orders</span></a>
-            <a href="/seller/ui/employees.php" class="nav-item"><i class="fas fa-chart-bar"></i><span>Employees</span></a>
+            <a href="/seller/ui/employees.php" class="nav-item"><i class="fas fa-users"></i><span>Employees</span></a>
             <a href="/seller/ui/analytics.php" class="nav-item active"><i class="fas fa-chart-bar"></i><span>Analytics</span></a>
             <a href="#" class="nav-item"><i class="fas fa-cog"></i><span>Settings</span></a>
         </nav>
